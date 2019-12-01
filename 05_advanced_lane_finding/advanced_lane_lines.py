5303c5c990ae5db2ec3f8dbb9c15156444138e91
AI.Tsingh.ChiangC <jiangchuna@126.com>
2019-12-01 21:50:08 +0800

    P2

---
+++ 05_advanced_lane_finding/advanced_lane_lines.py
@@ -0,0 +1,114 @@
+import cv2
+import numpy as np
+
+
+def warper(img, src, dst):
+    # Compute and apply perpective transform
+    img_size = (img.shape[1], img.shape[0])
+    M = cv2.getPerspectiveTransform(src, dst)
+    warped = cv2.warpPerspective(img, M, img_size, flags=cv2.INTER_NEAREST)  # keep same size as input image
+
+    return warped
+
+
+def abs_sobel_thresh(img, orient='x', sobel_kernel=3, thresh=(0, 255)):
+    # Calculate directional gradient
+    # Apply threshold
+    # 1) Convert to grayscale
+    gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+    
+    # 2) Take the derivative in x or y given orient = 'x' or 'y'
+    if orient == 'x':
+        sobel = cv2.Sobel(gray, cv2.CV_64F, 1, 0, sobel_kernel)
+    
+    if orient == 'y':
+        sobel = cv2.Sobel(gray, cv2.CV_64F, 0, 1, sobel_kernel)
+    
+    # 3) Take the absolute value of the derivative or gradient
+    abs_sobel = np.absolute(sobel)
+    
+    # 4) Scale to 8-bit (0 - 255) then convert to type = np.uint8
+    scale_factor = np.max(abs_sobel)/255
+    scaled_sobel = np.uint8(abs_sobel/scale_factor)
+    
+    # 5) Create a mask of 1's where the scaled gradient magnitude 
+            # is > thresh_min and < thresh_max
+    grad_binary = np.zeros_like(scaled_sobel)
+    grad_binary[(scaled_sobel >= thresh[0]) & (scaled_sobel <= thresh[1])] = 1
+    
+    # 6) Return this mask as your grad_binary image
+    return grad_binary
+
+def mag_thresh(image, sobel_kernel=3, mag_thresh=(0, 255)):
+    # Calculate gradient magnitude
+    # Apply threshold
+    
+    #1 Convert to grayscale
+    gray = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
+    
+    #2 Take the gradient in x and y separately
+    sobelx = cv2.Sobel(gray, cv2.CV_64F, 1, 0, sobel_kernel)
+    sobely = cv2.Sobel(gray, cv2.CV_64F, 0, 1, sobel_kernel)
+    
+    #3 Calculate the gradient magnitude
+    grad_mag = np.sqrt(sobelx**2, sobely**2)
+    
+    #4 Rescale to 8 bit
+    scale_factor = np.max(grad_mag)/255
+    grad_mag = (grad_mag/scale_factor).astype(np.uint8)
+    
+    #5 Create a binary image of ones where threshold is met, zeros otherwise
+    mag_binary = np.zeros_like(grad_mag)
+    mag_binary[(grad_mag >= mag_thresh[0]) & (grad_mag <= mag_thresh[1])] = 1
+    
+    
+    return mag_binary
+
+#
+def dir_threshold(image, sobel_kernel=3, thresh=(0, np.pi/2)):
+    # Calculate gradient direction
+    # Apply threshold
+    # 1) Convert to grayscale
+    gray = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
+    
+    # 2) Take the gradient in x and y separately
+    sobelx = cv2.Sobel(gray, cv2.CV_64F, 1, 0, sobel_kernel)
+    sobely = cv2.Sobel(gray, cv2.CV_64F, 0, 1, sobel_kernel)
+    
+    # 3) Take the absolute value of the x and y gradients
+    abs_sobelx = np.absolute(sobelx)
+    abs_sobely = np.absolute(sobely)
+    
+    # 4) Use np.arctan2(abs_sobely, abs_sobelx) to calculate the direction of the gradient 
+    abs_grad_dir = np.arctan2(abs_sobely, abs_sobelx)
+    
+    # 5) Create a binary mask where direction thresholds are met
+    dir_binary = np.zeros_like(abs_grad_dir)
+    
+    # 6) Return this mask as your dir_binary image
+    dir_binary[(abs_grad_dir >= thresh[0])&(abs_grad_dir <= thresh[1])] = 1
+    
+    return dir_binary
+
+
+def hls_select(img, thresh=(0, 255)):
+    # 1) Convert to HLS color space
+    hls = cv2.cvtColor(img, cv2.COLOR_RGB2HLS)
+    # 2) Apply a threshold to the S channel
+    S = hls[:,:,2]
+    binary_output = np.zeros_like(S)
+    binary_output[(S >= thresh[0]) & (S <= thresh[1])] = 1
+    # 3) Return a binary image of threshold result
+    return binary_output
+
+
+
+
+
+
+
+
+
+
+
+
