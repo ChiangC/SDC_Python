a656d4812b4ceb9964a2aae6ad3253325c588868
AI.Tsingh.ChiangC <jiangchuna@126.com>
2020-01-05 19:56:07 +0800

    behavioral cloning final shot.

--- 07_behavioral_cloning/model.py
+++ 07_behavioral_cloning/model.py
@@ -1,146 +1,138 @@
 import csv
 import cv2
 import numpy as np
+import os
+from math import ceil 
+from PIL import Image
+import random
 
-from keras.models import Seqential
-from keras.layers import Flatten, Dese, Lambda,Cropping2D
+from keras.models import Sequential
+from keras.layers import Flatten, Dense, Lambda, Cropping2D, Conv2D, Dropout
 from keras.layers.convolutional import Convolution2D
 from keras.layers.pooling import MaxPooling2D
-
-lines=[]
-with open('./data/driving_log.csv') as csvfile:
-    reader = csv.reader(csvfile)
-    for line in reader:
-        lines.append(line)
-
-images = []
-measurements = []
-for line in lines:
-    for i in range(3):
-        source_path = line[i]
-        filename = source_path.split('/')[-1]
-        current_path = './data/IMG/'+filename
-        image = cv2.imread(current_path)
-        images.append(image)
-        measurement = float(line[3])
-        measurements.append(measurement)
-
-#Data augmentation        
-augmented_images, augmented_measurements = [],[]
-for image, measurement in zip(images, measurements):
-    augmented_images.append(image)
-    augmented_measurements.append(measurement)
-    augmented_images.append(cv2.flip(image, 1))
-    augmented_measurements.append(measurement*-1.0)
-
-X_train = np.array(augmented_images)
-y_train = np.array(augmented_measurements)
-
+from keras.models import Model
+import sklearn
+from sklearn.model_selection import train_test_split
+from sklearn.utils import shuffle
+    
+        
+########################################################################
+#Model Architecture
+########################################################################
 #Use Keras to train a network to do the following:
 #1.Take in an image from the center camera of the car. This is the input to your neural network.
 #2.Output a new steering angle for the car. 
 
 model = Sequential()
-# set up lambda layer
-model.add(Lambda(lambda x:x/255.0 - 0.5, input_shape=(160, 320, 3))
+#Set up lambda layer
+model.add(Lambda(lambda x: x/255.0 - 0.5, input_shape=(160, 320, 3)))
 #Cropping2D Layer
-model.add(Copping2D(cropping=((70,25),(0,0))))
-# model.add(Cropping2D(cropping=((50,20), (0,0)), input_shape=(160,320,3)))
+# model.add(Cropping2D(cropping=((70,25),(0,0))))
+model.add(Cropping2D(cropping=((50,20), (0,0)), input_shape=(160,320,3)))
 
-#LeNet
-# model.add(Convolution2D(6, 5, 5, activation="relu"))
-# model.add(MaxPooling2D())
-# model.add(Convolution2D(6, 5, 5, activation="relu"))
-# model.add(MaxPooling2D())
-# model.add(Flatten())
-# model.add(Dense(120))
-# model.add(Dense(84))
-# model.add(Dense(1))
+model.add(Conv2D(24, (5, 5), strides=(2, 2), activation="relu"))
+model.add(Conv2D(36, (5,5), strides=(2,2), activation="relu"))
+model.add(Conv2D(48, (5,5), strides=(2,2), activation="relu"))
+model.add(Conv2D(64, (3,3), activation="relu"))
+model.add(Conv2D(64, (3,3), activation="relu"))
+model.add(Flatten())
+model.add(Dense(100))
+model.add(Dropout(0.5))
+model.add(Dense(50))
+# model.add(Dropout(0.5))
+model.add(Dense(10))
+# model.add(Dropout(0.5))
+model.add(Dense(1))
+
+########################################################################
+#Data augmentation        
+########################################################################
+def data_augment(images, measurements):
+    augmented_images, augmented_measurements = [],[]
+    for image, measurement in zip(images, measurements):
+        augmented_images.append(image)
+        augmented_measurements.append(measurement)
+        augmented_images.append(cv2.flip(image, 1))
+        augmented_measurements.append(measurement*-1.0)
+
+    return augmented_images,augmented_measurements
+
+########################################################################
+#Generator
+########################################################################
+def generator(samples, batch_size=32):
+    num_samples = len(samples)
+    while 1: # Loop forever so the generator never terminates
+        shuffle(samples)
+        for offset in range(0, num_samples, batch_size):
+            batch_samples = samples[offset:offset+batch_size]
+
+            images = []
+            angles = []
+            for row in batch_samples:
+                steering_center = float(row[3])
+
+                # create adjusted steering measurements for the side camera images
+                correction = 0.1 # this is a parameter to tune
+                steering_left = steering_center + correction
+                steering_right = steering_center - correction
+                
+#                 steering_left = steering_center
+#                 steering_right = steering_center
+                # read in images from center, left and right cameras
+                path = "./data/IMG/" 
+                img_center = np.asarray(Image.open(path + row[0].split('\\')[-1]))
+                img_left = np.asarray(Image.open(path + row[1].split('\\')[-1]))
+                img_right = np.asarray(Image.open(path + row[2].split('\\')[-1]))
+
+                # add images and angles to data set
+                images.append(img_center)
+                images.append(img_left)
+                images.append(img_right)
+                angles.append(steering_center)
+                angles.append(steering_left)
+                angles.append(steering_right)
+#                 images.extend(img_center, img_left, img_right)
+#                 angles.extend(steering_center, steering_left, steering_right)
+
+            augmented_images, augmented_measurements = data_augment(images, angles)
+            X_train = np.array(augmented_images)
+            y_train = np.array(augmented_measurements)
+            yield sklearn.utils.shuffle(X_train, y_train)
+            
+            
+########################################################################
+########################################################################
+samples=[]
+with open('./data/driving_log.csv') as csvfile:
+    reader = csv.reader(csvfile)
+    for line in reader:
+        samples.append(line)
+        
+train_samples, validation_samples = train_test_split(samples, test_size=0.3)
+
+batch_size = 32
+
+#Compile and train the model using the generator function
+train_generator = generator(train_samples, batch_size=batch_size)
+validation_generator = generator(validation_samples, batch_size=batch_size)          
           
-          
-          
+model.summary()   
+
 model.compile(loss='mse', optimizer='adam')
-model.fit(X_train, y_train, validation_split=0.2, shuffle=True, nb_epoch=5)
+# model.fit(X_train, y_train, validation_split=0.2, shuffle=True, epochs=5)
+history_object = model.fit_generator(train_generator, 
+                                     steps_per_epoch=ceil(len(train_samples)/batch_size),
+                                     validation_data=validation_generator, 
+                                     validation_steps=ceil(len(validation_samples)/batch_size), 
+                                     epochs=5, verbose=1)
+
+
 
 model.save("model.h5")
 
 
-# with open(csv_file, 'r') as f:
-#     reader = csv.reader(f)
-#     for row in reader:
-#         steering_center = float(row[3])
-
-#         # create adjusted steering measurements for the side camera images
-#         correction = 0.2 # this is a parameter to tune
-#         steering_left = steering_center + correction
-#         steering_right = steering_center - correction
-
-#         # read in images from center, left and right cameras
-#         path = "..." # fill in the path to your training IMG directory
-#         img_center = process_image(np.asarray(Image.open(path + row[0])))
-#         img_left = process_image(np.asarray(Image.open(path + row[1])))
-#         img_right = process_image(np.asarray(Image.open(path + row[2])))
-
-#         # add images and angles to data set
-#         car_images.extend(img_center, img_left, img_right)
-#         steering_angles.extend(steering_center, steering_left, steering_right)
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+          
+          
---
+++ 07_behavioral_cloning/run_model.bat
@@ -0,0 +1,4 @@
+
+cd /d E:\SDC_Pros\SDC_Python\07_behavioral_cloning
+
+python drive.py model.h5
